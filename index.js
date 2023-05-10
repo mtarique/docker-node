bae2b3531b2395f660c68ad2c71023a8cd73073f
mtarique <mtarique@outlook.com>
2023-05-10 12:06:05 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+
+const app = express(); 
+const port = 5000; 
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true})); 
+
+app.use('/', (req, res) => {
+    res.json({message: `API is running successfully on ${port}...`})
+}); 
+
+app.listen(5000, () => {
+    console.log(`Application is running on port ${port}`); 
+});
